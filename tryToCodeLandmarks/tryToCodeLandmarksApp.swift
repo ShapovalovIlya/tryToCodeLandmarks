327b7c7e81e6f9083598b507427c4bb4f29cc4f8
Ilia <greateman@mail.ru>
2021-11-12 23:30:21 +0300

    Initial Commit

---
+++ tryToCodeLandmarks/tryToCodeLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  tryToCodeLandmarksApp.swift
+//  tryToCodeLandmarks
+//
+//  Created by Илья Шаповалов on 12.11.2021.
+//
+
+import SwiftUI
+
+@main
+struct tryToCodeLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
