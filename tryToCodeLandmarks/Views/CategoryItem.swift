2b441feea991730228b20ddb4d2c9cd5cb8de357
Shapovalov_Ilay <greateman@mail.ru>
2021-12-09 00:10:17 +0300

    Added complex interfaces

---
+++ tryToCodeLandmarks/Views/CategoryItem.swift
@@ -0,0 +1,20 @@
+//
+//  CategoryItem.swift
+//  tryToCodeLandmarks
+//
+//  Created by Илья Шаповалов on 08.12.2021.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct CategoryItem_Previews: PreviewProvider {
+    static var previews: some View {
+        CategoryItem()
+    }
+}
