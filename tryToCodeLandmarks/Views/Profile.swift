b47cb5041f1628d64e0477abdca4145bb975c230
Shapovalov_Ilay <greateman@mail.ru>
2021-12-20 08:46:11 +0300

    add complexs interfaces

---
+++ tryToCodeLandmarks/Views/Profile.swift
@@ -0,0 +1,27 @@
+//
+//  Profile.swift
+//  tryToCodeLandmarks
+//
+//  Created by Илья Шаповалов on 17.12.2021.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+    
+    static let `default` = Profile(username: "g_kumar")
+    
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+        
+        var id: String { rawValue }
+    }
+}
+
